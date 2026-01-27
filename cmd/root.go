cd4dcfa212b09911b91b405baf3c413084d97afb
Rasyid Hakim <rasyid.xyz@gmail.com>
2026-01-27 09:13:52 +0700

    refactor: follow cobra app structure

---
+++ cmd/root.go
@@ -0,0 +1,19 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "gym",
+	Short: "gym manages synchronization of agent skills into projects",
+}
+
+// Execute runs the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd())
+	rootCmd.AddCommand(addCmd())
+	rootCmd.AddCommand(syncCmd())
+}
